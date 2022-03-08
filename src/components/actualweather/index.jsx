2c6c573d74cfa61425aea2c8dcc6b4416726d05d
Robinbucad <robin.bucad6@gmail.com>
2022-03-08 21:00:50 +0100

    bootstrap

--- src/components/actualweather/index.jsx
+++ src/components/actualweather/index.jsx
@@ -3,39 +3,59 @@ import { useState, useEffect } from 'react'
 import { useActualWeather } from '../../custom-hooks/hook-actualWeather'
 import { useSearchWeather } from '../../custom-hooks/hook-search'
 import './style.css'
+import { Card, Button } from 'react-bootstrap'
 
 function CurrentActualWeather() {
 
 
-    const [unix,setUnix] = useState([])
-  
+    const [unix, setUnix] = useState([])
+
 
     const actual = useActualWeather([])
-   
+
 
     const search = useSearchWeather('madrid')
     console.log(search)
 
-    
 
 
-    
 
-    return ( 
+
+
+    return (
         <div className='container'>
-           {actual.map((e,i) => (
-               <div className='card' key={i}>
-                   <p>{e.dt}</p>
-                   <img src={`https://openweathermap.org/img/wn/${e.weather.map(e => e.icon)}@2x.png`} />
-                   <p>º{e.temp.day}</p>
-                </div>
-               
-          
-           ))}
+            {actual.map((e, i) => (
+                <Card style={{ width: '18rem' }}>
+                    
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                           {e.dt}
+                        </Card.Text>
+                        <Card.Text>{e.temp.day}</Card.Text>
+                        <Card.Img variant="top" src={`https://openweathermap.org/img/wn/${e.weather.map(e => e.icon)}@2x.png`} />
+                    </Card.Body>
+                </Card>
+            ))}
+
+
         </div>
-     )
+    )
 }
 
 
 export default CurrentActualWeather
+
+/**
+ * 
+ *  {actual.map((e, i) => (
+                <div className='card' key={i}>
+                    <p>{e.dt}</p>
+                    <img src={`https://openweathermap.org/img/wn/${e.weather.map(e => e.icon)}@2x.png`} />
+                    <p>º{e.temp.day}</p>
+                </div>
+
+
+            ))}
+ */
 
