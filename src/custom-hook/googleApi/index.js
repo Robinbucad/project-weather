3d356a778eac0d11d1bfed4ef8ac7690de6bb0bb
Robinbucad <robin.bucad6@gmail.com>
2022-03-11 16:20:11 +0100

    d

--- src/custom-hook/googleApi/index.js
+++ src/custom-hook/googleApi/index.js
@@ -12,7 +12,7 @@ export const usePlaces = e => {
 
 
     useEffect(() => {
-        fetch('')
+        fetch(fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.3890815,-0.5352448&radius=1500&type=restaurant&key=AIzaSyDU_rTJMBQ3rp8BRMiqFb9_sgd4AsxOJ-E'))
             .then(r => r.json())
             .then(d => {
           
